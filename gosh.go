514e509e2d57593506a1817355a186db711a0eb4
Mike Clarke <michaelclarkecs@gmail.com>
2016-02-20 21:47:21 +0000

    Basic shell by reading from stdin

--- gosh.go
+++ gosh.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	config := LoadConfig()
 
-	fmt.Printf(LocateCommand("ls", config.Path))
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		command := strings.Split(strings.TrimSpace(text), " ")[0]
+		fmt.Printf("%s\n", LocateCommand(command, config.Path))
+	}
 }
